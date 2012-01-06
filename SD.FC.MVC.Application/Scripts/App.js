3f20c5dd54eca614395a3a309935e2719f73073e
Sergey.Dzyuban <dzyuban@gmail.com>
2012-01-07 01:47:46 +0200

    facebookclient

---
+++ SD.FC.MVC.Application/Scripts/App.js
@@ -0,0 +1,26 @@
+﻿var App =
+{
+    initialize: function () {
+        $("#Friends_btnCheckAllFriends").click(function () {
+            var checked_status = "checked";
+            $("input[name=chkFriends]").each(function () {
+                this.checked = checked_status;
+            });
+        });
+
+        $("#Friends_btnUnCheckAllFriends").click(function () {
+            var checked_status = "";
+            $("input[name=chkFriends]").each(function () {
+                this.checked = checked_status;
+            });
+        });
+    }
+
+};
+
+$(document).ready(function () {
+    App.initialize();
+});
+
+
+
